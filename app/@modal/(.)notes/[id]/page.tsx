fbf1dbd009d00dd743c9eb08b3351d3445364cb7
Ira <semikirina@ukr.net>
2025-08-10 10:19:00 +0200

    change location

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,4 +1,4 @@
-import NotePreview from '@/components/NotePreview/NotePreview';
+import NotePreview from '@/app/@modal/(.)notes/[id]/NotePreview.client';
 
 type Props = { params: Promise<{ id: string }> };
 
